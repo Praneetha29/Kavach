50611036adbe317c2c08cd8e1e8b4ec7923ccf0c
Praneetha29 <praneetha2829@gmail.com>
2023-11-22 23:10:24 +0530

    add:markers enabled in map

--- kavach_app/lib/Screens/map_screen.dart
+++ kavach_app/lib/Screens/map_screen.dart
@@ -16,16 +16,46 @@ class _MapScreenState extends State<MapScreen> {
   final CameraPosition _kGooglePlex =
       const CameraPosition(target: LatLng(22.2530, 84.9016), zoom: 14.4746);
 
+  List<Marker> _marker = [];
+  List<Marker> _list = const [
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(22.2530, 84.9016),
+        infoWindow: InfoWindow(title: 'My current location'))
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _marker.addAll(_list);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _kGooglePlex,
-        mapType: MapType.normal,
-        myLocationEnabled: false,
-        compassEnabled: true,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          markers: Set<Marker>.of(_marker),
+          mapType: MapType.normal,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.location_disabled_outlined),
+        onPressed: ()async{
+          GoogleMapController controller = await _controller.future ;
+          controller.animateCamera(CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(35.672, 139.6502),
+              zoom: 14
+            )
+          ));
+          setState(() {
+
+          });
         },
       ),
     );
