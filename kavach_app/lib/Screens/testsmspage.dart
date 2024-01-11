57d3357962a6bb2ac83adb7e073b90a9265dc494
Praneetha29 <praneetha2829@gmail.com>
2024-01-11 11:54:41 +0530

    feat: Manual SMS sent

--- kavach_app/lib/Screens/testsmspage.dart
+++ kavach_app/lib/Screens/testsmspage.dart
@@ -1,49 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-// Function to launch the SMS app with pre-filled message
-Future<void> sendTestSMS() async {
-  const String phoneNumber = '+918309196050';
-  const double startPointLat = 12.9716; // Example start latitude (Bangalore)
-  const double startPointLong = 77.5946; // Example start longitude (Bangalore)
-  const double endPointLat = 13.0016; // Example end latitude (~3km away)
-  const double endPointLong = 77.6146; // Example end longitude (~3km away)
-  const String landmark = "near the ABC Landmark"; // Example landmark
+class SendMessageWidget extends StatelessWidget {
+  final String phoneNumber = '+918309196050';
+  final String destinationLatitude = '22.253469'; // NIT Rourkela latitude
+  final String destinationLongitude = '84.901132'; // NIT Rourkela longitude
 
-  // Create the Google Maps URL for the route
-  String mapsUrl = 'https://www.google.com/maps/dir/?api=1'
-      '&origin=$startPointLat,$startPointLong'
-      '&destination=$endPointLat,$endPointLong'
-      '&travelmode=driving';
-
-  // Construct the message
-  String message = Uri.encodeComponent('Emergency! An accident has occurred $landmark. Follow this route for help: $mapsUrl');
-
-  // Construct the URI for SMS
-  String smsUri = 'sms:$phoneNumber?body=$message';
-
-  // Check if the SMS app can be launched
-  if (await canLaunch(smsUri)) {
-    await launch(smsUri); // Launch the SMS app
-  } else {
-    print('Failed to send SMS');
+  // Function to generate the Google Maps URL for a mock route
+  String createGoogleMapsUrl() {
+    // Mock current latitude and longitude
+    String mockCurrentLatitude = '37.4220041';
+    String mockCurrentLongitude = '-122.0862462';
+    return 'https://www.google.com/maps/dir/?api=1&travelmode=driving&dir_action=navigate&destination=$destinationLatitude,$destinationLongitude&origin=$mockCurrentLatitude,$mockCurrentLongitude';
   }
-}
-class TestSmsPage extends StatelessWidget {
+
+  // Function to launch the SMS app with a pre-filled message
+  Future<void> sendSMS() async {
+    final String mapsUrl = createGoogleMapsUrl();
+    final String message = 'Follow this link to see the route: $mapsUrl';
+    final Uri smsUri = Uri.parse('sms:$phoneNumber?body=${Uri.encodeComponent(message)}');
+
+    if (await canLaunch(smsUri.toString())) {
+      await launch(smsUri.toString());
+    } else {
+      throw 'Could not launch SMS app';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Test SMS"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            sendTestSMS();
-          },
-          child: Text('Send Test SMS'),
-        ),
-      ),
+    return ElevatedButton(
+      onPressed: sendSMS,
+      child: Text('Test SMS'),
     );
   }
 }
